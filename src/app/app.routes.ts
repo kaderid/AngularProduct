a27cdcdfce9e40ecdf4932580402db65a095059c
Abdoulkader Boubacar IDDE <abdoulkader.bi@kuikops.com>
2025-10-01 17:44:23 +0200

    Angular Product preject creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Produits } from './produits/produits';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "produits", component : Produits}
+];
---
+++ src/app/produits/produits.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  imports: [],
+  templateUrl: './produits.html',
+})
+export class Produits {
+
+}
