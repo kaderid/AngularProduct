da7b307b964a0273bef1c4303bb8d7995d73bb3f
Kader <abdoulkaderidde@gmail.com>
2025-10-02 11:14:43 +0200

    Ajout de composant

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Produits } from './produits/produits';
+import { AddProduit } from './add-produit/add-produit';
 
 export const routes: Routes = [
-    {path: "produits", component : Produits}
+    {path: "produits", component : Produits},
+    {path: "add-produit", component : AddProduit}
 ];
