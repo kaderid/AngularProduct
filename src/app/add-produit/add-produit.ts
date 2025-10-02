0e1416bbf3559fc9e92afc7372fc7691d4901605
Kader <abdoulkaderidde@gmail.com>
2025-10-02 16:37:51 +0200

    Creation d'un formulaire pour l'ajout d'un produit

--- src/app/add-produit/add-produit.ts
+++ src/app/add-produit/add-produit.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Produit } from '../model/produit.model';
 
 @Component({
   selector: 'app-add-produit',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-produit.html',
 })
-export class AddProduit {
+export class AddProduit implements OnInit{
+
+  newProduit = new Produit();
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  addProduit(){ 
+    console.log(this.newProduit); 
+  }
 
 }
